d02b60051a2ea5c71d1541e3641f15f651dd7dec
Sujit Maharjan <Sujit.Maharjan@logpoint.com>
2020-06-04 23:34:58 +0545

    combined cpp and COA lab site

---
+++ app.js
@@ -0,0 +1,80 @@
+var app = angular.module('labapp',['ngRoute']);
+
+app.controller('labListController', function ($scope, $route, $routeParams, $location, $http) {
+    $scope.labs = {};
+    $scope.data = {};
+    $scope.state = {};
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+    var data = $scope.data;
+    function reset()
+    {
+        if($routeParams.lab)
+        {
+            $scope.getLab($routeParams.lab);
+            if($routeParams.labId)
+            {
+                $scope.labfileDisplay($routeParams.labId);
+                if($routeParams.qNo)
+                {
+                    $scope.codeDisplay($routeParams.qNo);
+                }
+                else
+                {
+                    $scope.codeDisplay(0);
+                }
+            }
+            else
+            {
+                $scope.labfileDisplay(0);
+                $scope.codeDisplay(0);
+            }
+        }
+        else
+        {
+            $scope.getLab(Object.keys($scope.labs)[0]);
+            $scope.labfileDisplay(0);
+            //$scope.codeDisplay(0);
+        }
+    };
+
+    $scope.getLab = function(lab)
+    {
+        $scope.state.labname = lab;
+        $http.get($scope.labs[lab]["path"]+"/api.json").success(function(response) {
+            $scope.data = response;
+        });
+        $location.path("/"+$scope.state.labname+"/"+lab);
+    };
+    $scope.labfileDisplay = function(labno)
+    {
+        $scope.state.displayLab = labno;
+        $scope.state.filename = "";
+        $location.path("/"+$scope.state.labname+"/"+labno);
+    };
+    $scope.codeDisplay = function(qNo)
+    {
+        $scope.state.filename = $scope.labs[$scope.state.labname].path+ "/" + $scope.data.labs[$scope.state.displayLab].files[qNo].src;
+        $http.get($scope.state.filename).success(function(response) {
+            $scope.state.filecontent = response;
+        });
+        $location.path("/" + $scope.state.labname+"/"+$scope.state.displayLab+"/"+qNo);
+    };
+    $http.get('/api.json').success(function(response) {
+        $scope.labs = response;
+        reset();
+    });
+}
+              );
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/:lab/:labId/:qNo",{})
+        .when("/:lab/:labId",{})
+        .when("/:lab",{})
+        .when("/",{});
+}
+          );
+
+
--- cpp/lab3/employee.cpp
+++ cpp/lab3/employee.cpp
@@ -7,7 +7,7 @@
  * Assume that an object represents an employee report that contains the information like employee id, total bonus, total overtime in a particular year. Use  array of objects to represent n employees' reports. Write a program that displays report. Use setpara() member function to set report attributes by passing the arguments and member function displayreport() to show the reports according to parameter passed. Display the report in following format. 
  *
  *   Employee with ... ... ... has received Rs ... ... ...as bonus and
-    had worked ... ... ... hours as a over time in  year ... ... ... 
+ had worked ... ... ... hours as a over time in  year ... ... ... 
  *
  */
 #include <iostream>
